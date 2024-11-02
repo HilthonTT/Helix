44c32cbcc1d99b54ceab77f53e86157a59e74825
Timothy Tandt <Timothy.tandt@outlook.be>
2024-11-02 12:28:41 +0100

    issue: fixed json error

--- Helix.Application/Core/Errors/JsonErrors.cs
+++ Helix.Application/Core/Errors/JsonErrors.cs
@@ -5,5 +5,5 @@ namespace Helix.Application.Core.Errors;
 internal static class JsonErrors
 {
     public static readonly Error Invalid = Error.Problem(
-        "Json.Invalid", "Your valid is valid.");
+        "Json.Invalid", "Your json format is invalid.");
 }
